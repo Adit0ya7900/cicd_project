28fdad1f14a4f0f5019b4699855d4ae81e27c364
Dheeru <dheeru@Dheerus-MacBook-Air.local>
2026-04-29 20:33:45 +0530

    Final CI/CD test

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Auto deployment from Github Actions  🚀"
+    return "my cicd pipeline works successfully  🚀"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
